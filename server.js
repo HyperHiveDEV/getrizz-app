923088ab86234346dae53af7d9ef1f815762eba6
HyperHive DEV <hyperhivecontact@gmail.com>
2026-03-16 21:50:21 +0100

    fix port for railway

--- server.js
+++ server.js
@@ -38,4 +38,6 @@ res.json(data);
   }
 });
 
-app.listen(3001, '0.0.0.0', () => console.log('API server running on port 3001'));
+const PORT = process.env.PORT || 3001;
+const server = app.listen(PORT, '0.0.0.0', () => console.log(`API server running on port ${PORT}`));
+server.timeout = 60000;
